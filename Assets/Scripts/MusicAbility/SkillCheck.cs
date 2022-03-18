b569b51f6d79b49d628d8f09f63ce1bf2696fc9f
owenmagri24 <magriowen2410@gmail.com>
2022-03-18 09:12:50 +0100

    Started work on fixing skillcheck
    -- Try to put skill check function on notes

--- Assets/Scripts/MusicAbility/SkillCheck.cs
+++ Assets/Scripts/MusicAbility/SkillCheck.cs
@@ -1,34 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SkillCheck : MonoBehaviour
 {
+
+    public enum ControlKey
+    {
+        E = KeyCode.E,
+        Q = KeyCode.Q
+    }
+
     private bool note1inSkillCheck;
     private bool note2inSkillCheck;
+
+    public ControlKey key;
+
+    // public UnityEvent OnKeyPress = new UnityEvent();
 
     private void Start() {
         note1inSkillCheck = false;
         note2inSkillCheck = false;
+
+        // OnKeyPress.AddListener(PressedKey);
+        // OnKeyPress.Invoke();
     }
+
+    // void PressedKey()
+    // {
+    //     Debug.Log("Hurrah1");
+    // }
     
     private void Update() {
-        if(Input.GetKeyDown(KeyCode.Q))
+        if(Input.GetKeyDown((KeyCode) (int) key))
         {
             if(note1inSkillCheck)
-            {
-                //Succesful Skillcheck
-                Debug.Log("Success");
-            }
-            else
-            {
-                //Bad Skillcheck
-                Debug.Log("Failure");
-            }
-        }
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            if(note2inSkillCheck)
             {
                 //Succesful Skillcheck
                 Debug.Log("Success");
@@ -42,37 +49,11 @@ public class SkillCheck : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Note")
-        {
-            if(other.transform.position.y == gameObject.transform.position.y)
-            {
-                note1inSkillCheck = true;
-            }
-        }
-        else if(other.tag == "Note2")
-        {
-            if(other.transform.position.y == gameObject.transform.position.y)
-            {
-                note2inSkillCheck = true;
-            }
-        }
+        note1inSkillCheck = true;
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if(other.tag == "Note")
-        {
-            if(other.transform.position.y == gameObject.transform.position.y)
-            {
-                note1inSkillCheck = false;
-            }
-        }
-        else if(other.tag == "Note2")
-        {
-            if(other.transform.position.y == gameObject.transform.position.y)
-            {
-                note2inSkillCheck = false;
-            }
-        }
+        note1inSkillCheck = false;
     }
 }
